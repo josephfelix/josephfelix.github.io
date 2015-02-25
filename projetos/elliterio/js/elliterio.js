6e3732255ee117bd86c052f82c8c98a7201d91a9
Joseph Felix <joseph0xfff@gmail.com>
2015-02-25 11:21:12 -0300

    Enviando elliterio

---
+++ projetos/elliterio/js/elliterio.js
@@ -0,0 +1,64 @@
+/**
+* Desenvolvido por: Joseph F.
+* http://josephfelix.github.io
+*/
+$(document).ready(function()
+{   
+	  jQuery('.camera_wrap').camera(
+	  {
+			onEndTransition: function()
+			{
+				var animated = $('.cameracurrent .caption_animated');
+				if ( animated.length )
+				{
+					animated.each(function()
+					{
+						if ( !$(this).hasClass('animated') )
+						{
+							if ( window.innerWidth <= 800 && window.innerHeight <= 600 )
+							{
+								$(this).addClass('animated')
+								.addClass( $(this).data('easing') )
+								.css( { position: 'absolute', left: '10%', top: '20px' });
+							} else
+							{
+								$(this).addClass('animated')
+								.addClass( $(this).data('easing') )
+								.css( { position: 'absolute', left: $(this).data('x'), top: $(this).data('y') });
+							}
+							
+							if ( $(this).data( 'iteration' ) )
+							{
+								$(this).css({
+										'-moz-animation-iteration-count': $(this).data( 'iteration' ),
+										'-webkit-animation-iteration-count': $(this).data( 'iteration' ),
+										'-o-animation-iteration-count': $(this).data( 'iteration' ),
+										'animation-iteration-count': $(this).data( 'iteration' )
+									});
+							}		
+							if ( $(this).data( 'delay' ) )
+							{
+								$(this).css({
+										'-moz-animation-delay': $(this).data( 'delay' ),
+										'-webkit-animation-delay': $(this).data( 'delay' ),
+										'-o-animation-delay': $(this).data( 'delay' ),
+										'animation-delay': $(this).data( 'delay' )
+									});
+							}
+							
+							if ( $(this).data( 'duration' ) )
+							{
+								$(this).css({
+										'-moz-animation-duration': $(this).data( 'duration' ),
+										'-webkit-animation-duration': $(this).data( 'duration' ),
+										'-o-animation-duration': $(this).data( 'duration' ),
+										'animation-duration': $(this).data( 'duration' )
+									});
+							}
+							
+						}
+					});
+				}
+			}
+	  });
+}); 
